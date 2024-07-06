b94165c026042c007ae719a46ca0fea1a6557007
Nadim Mahmud Borad <nadim.ict20@gmail.com>
2024-07-06 08:31:24 +0600

    math

---
+++ Number theory/A. Odd Divisor.cpp
@@ -0,0 +1,24 @@
+// nadim [ict,mbstu]
+#include <bits/stdc++.h>
+using namespace std;
+#define tc int t;cin>>t;while(t--)
+#define yy cout<<"YES\n";
+#define nn cout<<"NO\n";
+#define ll long long int
+#define pb push_back
+
+int main() {
+   tc
+   {
+      ll n;
+      cin>>n;
+      if((n&(n-1))==0)
+      {
+        nn
+      }
+      else
+         yy
+   }
+   
+   return 0;
+}
