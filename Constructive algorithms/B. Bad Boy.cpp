5291158a3dc677aacfcad07040e9d1e3fd72a752
Nadim Mahmud Borad <nadim.ict20@gmail.com>
2024-07-13 11:35:58 +0600

    contest - 1537

---
+++ Constructive algorithms/B. Bad Boy.cpp
@@ -0,0 +1,19 @@
+// nadim [ict,mbstu]
+#include <bits/stdc++.h>
+using namespace std;
+#define tc int t;cin>>t;while(t--)
+#define yy cout<<"YES\n";
+#define nn cout<<"NO\n";
+#define ll long long int
+#define pb push_back
+
+int main() {
+   tc
+   {
+    int r,c,x,y;
+    cin>>r>>c>>x>>y;
+    cout<<1<<" "<<c<<" "<<r<<" "<<1<<endl;
+   }
+   
+   return 0;
+}
